9b6d278b22a4cda727767ea967f34ec31413bb41
kimnamsun <nsunny0908@naver.com>
2024-10-03 15:11:17 +0900

    style: Chip 디자인 수정

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,15 +1,24 @@
 import chipStyles from './style';
+import { Size } from '@/@types';
 import { theme } from '@/style';
 import styled from '@emotion/styled';
 import React from 'react';
 
-type ChipVariant = 'gray-filled' | 'primary-outline' | 'red-outline';
+type ChipVariant =
+  | 'primary-filled'
+  | 'primary-outline'
+  | 'primary-light-filled'
+  | 'gray-filled'
+  | 'red-outline';
+
+type ChipSize = Exclude<Size, 'md'>;
 
 export interface ChipProps {
   variant?: ChipVariant;
   children: React.ReactNode;
   startIcon?: React.ReactNode;
   endIcon?: React.ReactNode;
+  size?: ChipSize;
 }
 
 export const chipIconColors = {
@@ -22,11 +31,12 @@ export const Chip = ({
   startIcon,
   endIcon,
   children,
+  size = 'lg',
   ...rest
 }: ChipProps) => {
   return (
-    <ChipContainer variant={variant} {...rest}>
-      <ChipInner variant={variant}>
+    <ChipContainer variant={variant} size={size} {...rest}>
+      <ChipInner variant={variant} size={size}>
         {startIcon}
         {children}
         {endIcon}
@@ -38,6 +48,7 @@ export const Chip = ({
 const ChipContainer = styled('div')<{
   variant: ChipVariant;
   disabled?: boolean;
+  size?: ChipSize;
   onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
 }>`
   display: inline-flex;
@@ -45,23 +56,25 @@ const ChipContainer = styled('div')<{
   justify-content: center;
   white-space: nowrap;
   vertical-align: middle;
-  height: 22px;
-  border-radius: 999px;
+  border-radius: 100px;
   gap: ${({ theme }) => theme.spacing(2)};
-  padding: 2px 8px;
 
   ${({ variant }) => variant && chipStyles.variants[variant]};
+  ${({ size }) => size && chipStyles.size[size]};
 `;
 
 const ChipInner = styled('div')<{
   variant: ChipVariant;
   disabled?: boolean;
+  size?: ChipSize;
 }>`
   display: inline-flex;
   align-items: center;
   gap: 4px;
   white-space: nowrap;
-
-  ${({ theme }) => theme.typography.basic.medium};
   line-height: 16px;
+  ${({ size, theme }) =>
+    size === 'sm'
+      ? theme.typography.basic.medium
+      : theme.typography['basic-2'].medium};
 `;
--- src/components/Chip/style.ts
+++ src/components/Chip/style.ts
@@ -8,17 +8,36 @@ export const chipIconColors = {
 
 export default {
   variants: {
-    'gray-filled': css`
-      background: ${theme.palette.gray['100']};
-      color: ${theme.palette.gray['600']};
+    'primary-filled': css`
+      background-color: ${theme.palette.primary['300']};
+      color: ${theme.palette.white};
     `,
     'primary-outline': css`
       background-color: ${theme.palette.primary['20']};
       color: ${theme.palette.primary['300']};
+      outline: 1px solid ${theme.palette.primary['200']};
+    `,
+    'primary-light-filled': css`
+      background-color: ${theme.palette.primary['20']};
+      color: ${theme.palette.primary['300']};
+    `,
+    'gray-filled': css`
+      background: ${theme.palette.gray['100']};
+      color: ${theme.palette.gray['600']};
     `,
     'red-outline': css`
       background-color: ${theme.palette.error['50']};
       color: ${theme.palette.error['300']};
+    `,
+  },
+  size: {
+    sm: css`
+      padding: 2px 8px;
+      height: 22px;
+    `,
+    lg: css`
+      padding: 8px 14px;
+      height: 36px;
     `,
   },
 };
